13c548213ab8cd373243cd72a15f5a839e18c98a
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-06-17 19:08:10 +0800

    Add categories to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create title: "Drama"
+Category.create title: "Comedy"
+Category.create title: "Musical"
+Category.create title: "Romance"
+Category.create title: "Comedy"
+Category.create title: "Crime"
+Category.create title: "Action"
+Category.create title: "Thriller"
+Category.create title: "Horror"
+Category.create title: "Animation"
+Category.create title: "Cartoon"
+Category.create title: "Disaster"
+Category.create title: "Fantasy"
+Category.create title: "Western"
+Category.create title: "Art-house"
