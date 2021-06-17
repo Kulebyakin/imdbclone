a3ba1cfe3f089c6fe9988165fdd219514b3c4c49
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-06-17 22:48:49 +0800

    Add username to Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "movies#index"
 
-  devise_for :users
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', registration: 'register' }
+
   resources :movies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
