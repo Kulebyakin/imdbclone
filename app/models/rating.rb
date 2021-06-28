31e8ad5e266eacefa8d83a6e3fad0a344bb171b9
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-06-28 21:13:00 +0800

    Validate a rating by value from 1 to 10

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,6 @@
 class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 end
