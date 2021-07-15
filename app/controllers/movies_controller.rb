ef862bf98c0c0d144baa99b6b941363160f38fad
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-07-15 17:42:39 +0800

    Add ability to revoke your vote

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -43,13 +43,25 @@ class MoviesController < ApplicationController
 
     if rating >= 1 && rating <= 10
       if Movie.find(params[:id]).ratings.where(user: current_user).present?
-        ratings = Movie.find(params[:id]).ratings.where(user: current_user).update(rating: rating)
-        respond_to do |format|
-          if ratings
-            format.js
-            format.html { redirect_to movie_path, notice: "Movie was successfully rated." }
-          else
-            redirect_to movies_path, status: :unprocessable_entity
+        if Movie.find(params[:id]).ratings.where(user: current_user).pluck(:rating)[0] != rating
+          ratings = Movie.find(params[:id]).ratings.where(user: current_user).update(rating: rating)
+          respond_to do |format|
+            if ratings
+              format.js
+              format.html { redirect_to movie_path, notice: "Your rate was successfully changed." }
+            else
+              redirect_to movies_path, status: :unprocessable_entity
+            end
+          end
+        else
+          ratings = Movie.find(params[:id]).ratings.where(user: current_user).destroy(Movie.find(params[:id]).ratings.where(user: current_user).pluck(:id)[0])
+          respond_to do |format|
+            if ratings
+              format.js
+              format.html { redirect_to movie_path, notice: "Your rate was successfully revoked." }
+            else
+              redirect_to movies_path, status: :unprocessable_entity
+            end
           end
         end
       else
@@ -70,6 +82,7 @@ class MoviesController < ApplicationController
 
   # GET /movies/1 or /movies/1.json
   def show
+    @category = Category.joins(:movies).uniq
   end
 
   private
