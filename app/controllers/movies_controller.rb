1fcb2b451d3bb2ba35a838461550de8ed213383d
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-07-18 17:26:01 +0800

    Some fixes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,37 +1,38 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: :show
 
   # GET /movies or /movies.json
   def index
     @page = (params[:page] || 1).to_i
     @number_of_pages = (Movie.count / MOVIES_PER_PAGE.to_f).ceil
     
-    redirect_to movies_path, alert: "Wrong page number!" if @page > @number_of_pages || @page < 1
-    @movies = Movie.order(title: :asc).paginate(page: params[:page], per_page: MOVIES_PER_PAGE)
+    if @page > @number_of_pages || @page < 1
+      redirect_to movies_path, alert: "Wrong page number!" 
+    else
+      @movies = Movie.order(title: :asc).paginate(page: params[:page], per_page: MOVIES_PER_PAGE)
+    end
   end
 
   def category
     @page = (params[:page] || 1).to_i
     @cat = params[:category].capitalize
-    @number_of_pages = (Movie.joins(:category).where("categories.title = ?", @cat).count / MOVIES_PER_PAGE.to_f).ceil
+    category_movies = Movie.joins(:category).where("categories.title = ?", @cat)
+    @number_of_pages = (category_movies.count / MOVIES_PER_PAGE.to_f).ceil
 
-    if @page >= @number_of_pages && @page < 1
+    if @page > @number_of_pages || @page < 1
       redirect_to movies_path, alert: "Wrong page number!"
     else
-      @movies = Movie.joins(:category).where("categories.title = ?", @cat).paginate(page: params[:page], per_page: MOVIES_PER_PAGE)
+      @movies = category_movies.paginate(page: params[:page], per_page: MOVIES_PER_PAGE)
       render "index"
     end
   end
 
   # GET /movies/1 or /movies/1.json
   def show
+    @movie = Movie.find(params[:id])
+    @current_users_rating ||= @movie.ratings.where(user: current_user).pluck(:rating)[0]
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
 
     # Only allow a list of trusted parameters through.
     def movie_params
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -4,37 +4,44 @@ class RatingsController < ApplicationController
   def index
     rating = params[:rating].to_i
     @page = (params[:page] || 1).to_i
-    @cat = params[:category].capitalize if params[:category].present?
-    @number_of_pages = (Movie.count / MOVIES_PER_PAGE.to_f).ceil
 
-    redirect_to movies_path, alert: "Wrong page number!" if @page > @number_of_pages || @page < 1
-    
-    @movies = Movie.order(title: :asc).paginate(page: params[:page], per_page: MOVIES_PER_PAGE) if @cat.nil?
-    @movies = Movie.joins(:category).where("categories.title = ?", @cat).paginate(page: params[:page], per_page: MOVIES_PER_PAGE) if !@cat.nil?
-
-    if rating >= 1 && rating <= 10
-      my_rate = Movie.find(params[:id]).ratings.where(user: current_user)
-      if my_rate.present?
-        if my_rate.pluck(:rating)[0] != rating
-          ratings = my_rate.update(rating: rating)
-        else
-          ratings = my_rate.destroy(my_rate.pluck(:id)[0])
-        end
-      else
-        ratings = my_rate.build(rating: rating).save
-      end
-
-      respond_to do |format|
-        if ratings
-          format.js { render "movies/rate.js.erb" }
-          format.html { redirect_to movie_path, notice: "Your rate was successfully accepted." }
-        else
-          redirect_to movies_path, status: :unprocessable_entity
-        end
-      end
-
+    if params[:category].nil?
+      @number_of_pages = (Movie.count / MOVIES_PER_PAGE.to_f).ceil
+      @movies = Movie.order(title: :asc).paginate(page: params[:page], per_page: MOVIES_PER_PAGE)
     else
-      redirect_to movies_path, alert: "Wrong rate number!"
+      @cat = params[:category].capitalize 
+      category_movies = Movie.joins(:category).where("categories.title = ?", @cat)
+      @number_of_pages = (category_movies.count / MOVIES_PER_PAGE.to_f).ceil
+      @movies = category_movies.paginate(page: params[:page], per_page: MOVIES_PER_PAGE)
+    end
+
+    if @page > @number_of_pages || @page < 1
+      redirect_to movies_path, alert: "Wrong page number!" 
+    else
+      if rating < 1 || rating > 10
+        redirect_to movies_path, alert: "Wrong rate number!" 
+      else
+
+        users_rate = Movie.find(params[:id]).ratings.where(user: current_user)
+        if users_rate.present?
+          if users_rate.pluck(:rating)[0] == rating
+            ratings = users_rate.destroy(users_rate.pluck(:id)[0])
+          else
+            ratings = users_rate.update(rating: rating)
+          end
+        else
+          ratings = users_rate.build(rating: rating).save
+        end
+
+        respond_to do |format|
+          if ratings
+            format.js { render "movies/rate.js.erb" }
+            format.html { redirect_to movie_path, notice: "Your rate was successfully accepted." }
+          else
+            redirect_to movies_path, status: :unprocessable_entity
+          end
+        end
+      end
     end
   end
 
