f45a2482075771146769b3d10b176c890e0d8788
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-07-18 12:53:15 +0800

    Simplify ratings controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -8,46 +8,31 @@ class RatingsController < ApplicationController
     @number_of_pages = (Movie.count / MOVIES_PER_PAGE.to_f).ceil
 
     redirect_to movies_path, alert: "Wrong page number!" if @page > @number_of_pages || @page < 1
-    if @cat.nil?
-      @movies = Movie.order(title: :asc).paginate(page: params[:page], per_page: MOVIES_PER_PAGE)
-    else
-      @movies = Movie.joins(:category).where("categories.title = ?", @cat).paginate(page: params[:page], per_page: MOVIES_PER_PAGE)
-    end
+    
+    @movies = Movie.order(title: :asc).paginate(page: params[:page], per_page: MOVIES_PER_PAGE) if @cat.nil?
+    @movies = Movie.joins(:category).where("categories.title = ?", @cat).paginate(page: params[:page], per_page: MOVIES_PER_PAGE) if !@cat.nil?
 
     if rating >= 1 && rating <= 10
-      if Movie.find(params[:id]).ratings.where(user: current_user).present?
-        if Movie.find(params[:id]).ratings.where(user: current_user).pluck(:rating)[0] != rating
-          ratings = Movie.find(params[:id]).ratings.where(user: current_user).update(rating: rating)
-          respond_to do |format|
-            if ratings
-              format.js { render "movies/rate.js.erb" }
-              format.html { redirect_to movie_path, notice: "Your rate was successfully changed." }
-            else
-              redirect_to movies_path, status: :unprocessable_entity
-            end
-          end
+      my_rate = Movie.find(params[:id]).ratings.where(user: current_user)
+      if my_rate.present?
+        if my_rate.pluck(:rating)[0] != rating
+          ratings = my_rate.update(rating: rating)
         else
-          ratings = Movie.find(params[:id]).ratings.where(user: current_user).destroy(Movie.find(params[:id]).ratings.where(user: current_user).pluck(:id)[0])
-          respond_to do |format|
-            if ratings
-              format.js { render "movies/rate.js.erb" }
-              format.html { redirect_to movie_path, notice: "Your rate was successfully revoked." }
-            else
-              redirect_to movies_path, status: :unprocessable_entity
-            end
-          end
+          ratings = my_rate.destroy(my_rate.pluck(:id)[0])
         end
       else
-        ratings = Movie.find(params[:id]).ratings.where(user: current_user).build(rating: rating)
-        respond_to do |format|
-          if ratings.save
-            format.js { render "movies/rate.js.erb" }
-            format.html { redirect_to movie_path, notice: "Movie was successfully rated." }
-          else
-            redirect_to movies_path, status: :unprocessable_entity
-          end
+        ratings = my_rate.build(rating: rating).save
+      end
+
+      respond_to do |format|
+        if ratings
+          format.js { render "movies/rate.js.erb" }
+          format.html { redirect_to movie_path, notice: "Your rate was successfully accepted." }
+        else
+          redirect_to movies_path, status: :unprocessable_entity
         end
       end
+
     else
       redirect_to movies_path, alert: "Wrong rate number!"
     end
