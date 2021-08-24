597ef6744c202806672cb65de554c0d9a3590545
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-08-24 13:02:43 +0800

    Set pagination 10 movies per page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_category
 
-  MOVIES_PER_PAGE = 3
+  MOVIES_PER_PAGE = 10
 
   private
 
